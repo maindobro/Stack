73d1b1529147f0ba227afc1ff9941dab0d390f71
tpabahatp <nekros011@gmail.com>
2016-10-09 22:33:45 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -1,8 +1,7 @@
-
 #include <stack.cpp>
 #include <catch.hpp>
 #include <iostream>
-//using namespace std;
+using namespace std;
 
 SCENARIO("count", "[count]"){
   stack<int> s;
@@ -14,23 +13,14 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
-}
-
-SCENARIO("top", "[top]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
+  REQUIRE(s.pop()==1);
 }
 
 SCENARIO("pop", "[pop]"){
   stack<int> s;
   s.push(1);
-  s.push(2);
-  s.pop();
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
+  REQUIRE(s.pop()==1);
 }
 
 
@@ -40,20 +30,16 @@ SCENARIO("prisv", "[prisv]"){
   stack<int> s2;
   s2=s;
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
+  REQUIRE(s.pop()==1);
 }
 SCENARIO("copy", "[copy]"){
   stack<int> s;
   s.push(1);
   stack <int> a = s;
   REQUIRE(a.count()==1);
-  REQUIRE(a.top()==1);
+  REQUIRE(a.pop()==1);
 }
 SCENARIO("test", "[test]"){
   stack<int> s;
   REQUIRE(s.count()==0);
-}
-SCENARIO("empty", "[empty]"){
-  stack<int> s;
-  REQUIRE(s.empty()==true);
 }
