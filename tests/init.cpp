ca78360d4d7cb33319d88aa3c61a9d423eb2152e
tpabahatp <nekros011@gmail.com>
2016-11-06 21:21:57 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,27 +3,31 @@
 #include <iostream>
 using namespace std;
 
-SCENARIO("count", "[count]"){
+SCENARIO("count", "[count]")
+{
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
 }
 
-SCENARIO("push", "[push]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.count()==1);
-  REQUIRE(s.pop()==1);
-}
-
-SCENARIO("top", "[top]"){
+SCENARIO("push", "[push]")
+{
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
   REQUIRE(s.top()==1);
 }
 
-SCENARIO("pop", "[pop]"){
+SCENARIO("top", "[top]")
+{
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+  REQUIRE(s.top()==1);
+}
+
+SCENARIO("pop", "[pop]")
+{
   stack<int> s;
   s.push(1);
   s.push(2);
@@ -32,8 +36,8 @@ SCENARIO("pop", "[pop]"){
   REQUIRE(s.top()==1);
 }
 
-
-SCENARIO("prisv", "[prisv]"){
+SCENARIO("prisv", "[prisv]")
+{
   stack<int> s;
   s.push(1);
   stack<int> s2;
@@ -41,10 +45,18 @@ SCENARIO("prisv", "[prisv]"){
   REQUIRE(s.count()==1);
   REQUIRE(s.top()==1);
 }
-SCENARIO("copy", "[copy]"){
+
+SCENARIO("copy", "[copy]")
+{
   stack<int> s;
   s.push(1);
   stack <int> a = s;
   REQUIRE(a.count()==1);
   REQUIRE(a.top()==1);
+}
+
+SCENARIO("test", "[test]")
+{
+  stack<int> s;
+  REQUIRE(s.count()==0);
 }
