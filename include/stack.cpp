c507a3a4bbb5e2676503662a36dd16e2606ae7b8
tpabahatp <nekros011@gmail.com>
2016-11-06 20:12:09 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -4,20 +4,20 @@ template <typename T>
 class stack
 {
 public:
-	stack();							//noexcept
-	stack(stack const &);						//strong
-	~stack();							//noexcept
-	size_t count() const;						//noexcept
-	auto push(T const &) -> void;					//strong
-	T pop();							//basic
-	auto operator=(stack const & right)->stack &;			//strong
+	stack();							
+	stack(stack const &);						
+	~stack();							
+	size_t count() const;						
+	auto push(T const &) -> void;					
+	T pop();							
+	auto operator=(stack const & right)->stack &;			
 private:
 	T *array_;							
 	size_t array_size_;							
 	size_t count_;							
 };
 template<typename T>
-auto newcopy(const T * item, size_t size, size_t count) -> T*		//strong
+auto newcopy(const T * item, size_t size, size_t count) -> T*		
 {
 	T * buff = new T[size];
 	std::copy(item, item + count, buff);
