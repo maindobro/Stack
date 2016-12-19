278dc731c2418f139d58e070b5face72df36ed33
tpabahatp <nekros011@gmail.com>
2016-12-19 16:37:13 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,4 +1,4 @@
-#include <stack.cpp>
+#include <stack.hpp>
 #include <catch.hpp>
 #include <iostream>
 using namespace std;
@@ -13,27 +13,21 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
 }
 
 SCENARIO("pop", "[pop]"){
   stack<int> s;
-  s.push(1); s.pop();
+  s.push(1);
+  REQUIRE(*(s.pop())==1);	
   REQUIRE(s.count()==0);
 }
 
-SCENARIO("cop", "[cop]"){
+SCENARIO("prisv", "[prisv]"){
   stack<int> s;
   s.push(1);
-  stack<int> s2=s;
+  stack<int> s2;
+  s2=s;
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
-}
-
-SCENARIO("top", "[top]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.top()==1);
 }
 
 SCENARIO("empty", "[empty]"){
@@ -41,4 +35,25 @@ SCENARIO("empty", "[empty]"){
   s1.push(1);
   REQUIRE(!s1.empty());
   REQUIRE(s2.empty());
+}
+
+SCENARIO("threads", "[threads]"){
+  stack<int> s;
+  s.push(1);
+  s.push(2);
+  s.push(3);
+	std::thread t1([&s](){
+		for (int i = 0; i < 5; i++) {
+			s.push(i + 4);
+		}
+	});
+	std::thread t2([&s](){
+		for (int i = 0; i < 5; i++)
+		{
+			s.pop();
+		}
+	});
+	t1.join();
+	t2.join();
+  REQUIRE(s.count()==3);
 }
